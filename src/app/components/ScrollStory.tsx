b9fed45e634ca8af784050308857efca6dc48d92
chauhanarvind <chauhan.m.arvind@gmail.com>
2025-04-30 13:29:33 +1200

    working

--- src/app/components/ScrollStory.tsx
+++ src/app/components/ScrollStory.tsx
@@ -9,6 +9,27 @@ export default function ScrollStory() {
       </div>
       <div className="dialogue-box">
         <div className="p-elements">
+          <p className="narrator">But… Cody Cat?</p>
+          <p className="cody">Yeah?</p>
+          <p>
+            Have you noticed something…?You have fur. You must feel it more than
+            me!You have fur. You must feel it more than me!You have fur. You
+            must feel it more than me!You have fur. You must feel it more than
+            me!You have fur. You must feel it more than me!You have fur. You
+            must feel it more than me!
+          </p>
+          <p className="cody">What?</p>
+          <p>Umm… Now it’s getting hot!</p>
+          <p className="cody">Well, maybe a little…</p>
+          <p>You have fur. You must feel it more than me!</p>
+          <p>But… Cody Cat?</p>
+          <p className="cody">Yeah?</p>
+          <p>Have you noticed something…?</p>
+          <p className="cody">What?</p>
+          <p>Umm… Now it’s getting hot!</p>
+          <p className="cody">Well, maybe a little…</p>
+          <p>You have fur. You must feel it more than me!</p>
+          <p>You have fur. You must feel it more than me!</p>
           <p>But… Cody Cat?</p>
           <p className="cody">Yeah?</p>
           <p>Have you noticed something…?</p>
